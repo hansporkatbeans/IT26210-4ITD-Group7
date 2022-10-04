fa41ed66360d8e09458b6893318805d237348fba
NetAcad DEVASC <changeme@example.com>
2022-10-04 20:50:10 +0000

    Added Estimated Time of Arrival and Trip Duration Formatting.

--- MapQuest.py
+++ MapQuest.py
@@ -1,8 +1,38 @@
+from audioop import add
 import urllib.parse
 import requests
+import calendar
+from datetime import date, datetime, time, timedelta
 
 main_api = "https://www.mapquestapi.com/directions/v2/route?"
 key = "mvxR2JK4nufhenP6Yc3xjKeBBhFzqGBc" #Replace with your MapQuest key
+
+def realEta(time):
+    sec = float(time)
+    sec_value = sec % (24 * 3600)
+    hour_value = sec_value // 3600 
+    sec_value %= 3600
+    min = sec_value // 60
+    sec_value %= 60
+    travelTime = (datetime.now().strftime("%H:%M:%S %p"))
+    arrivalTime = str(((datetime.now() + timedelta(hours=hour_value, minutes=min, seconds=sec_value)).strftime("%H:%M:%S %p")))
+    print("Realtime Trip Duration: " + str("{:g}".format(hour_value)) + " Hours, " + str("{:g}".format(min)) + " Minutes, and " + str("{:g}".format(sec_value)) + " Seconds.")
+    print("Time of Travel: " + travelTime)
+    print("Time of Arrival: " + arrivalTime)
+
+    
+def estEta(time):
+    time = (json_data["route"]["formattedTime"])
+    date_time = datetime.strptime(time, "%H:%M:%S")
+    a_timedelta = date_time - datetime(1900, 1, 1)
+    sec = a_timedelta.total_seconds()
+    #sec = int(time%(24*3600))
+    hour = sec // 3600
+    sec %= 3600
+    min = sec // 60
+    sec %= 60
+    print("Estimated Trip Duration: " + str("{:g}".format(hour)) + " Hours, " + str("{:g}".format(min)) + " Minutes, and " + str("{:g}".format(sec)) + " Seconds.")
+
 
 while True:
    orig = input("Starting Location: ")
@@ -19,13 +49,12 @@ while True:
         print("API Status: " + str(json_status) + " = A successful route call.\n")
         print("=============================================")
         print("Directions from " + (orig) + " to " + (dest))
-        print("Trip Duration:   " + (json_data["route"]["formattedTime"]))
+        estEta(json_data["route"]["formattedTime"])  
+        realEta(json_data["route"]["realTime"])      
         print("Kilometers:      " + str("{:.2f}".format((json_data["route"]["distance"])*1.61)))
         print("Fuel Used (Ltr): " + str("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78)))
-        print("=============================================")
         for each in json_data["route"]["legs"][0]["maneuvers"]:
-            print((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)"))
-            print("=============================================\n")    
+            print((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)"))  
    elif json_status == 402:
         print("**********************************************")
         print("Status Code: " + str(json_status) + "; Invalid user inputs for one or both locations.")
